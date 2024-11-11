7a5d86f372a961f488d06226b092921a06760f52
iphone-samsung <rjsdn6422@naver.com>
2024-11-11 14:00:35 +0900

    Add files via upload

---
+++ PrintTest.java
@@ -0,0 +1,16 @@
+package j01_출력;
+
+public class PrintTest {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, Java!");
+		System.out.println("홍길동");
+		System.out.print("안녕\n");
+		System.out.println("난 길동이야");
+
+		// 주석 : 코드에 영향을 주지 않는 문자
+		// 실행 단축키 :ctrl +f11
+		// 자동 완성 단축키 : ctrl + spacebar
+	}
+
+}
